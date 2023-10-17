99d820c11afb180f3718acb13136e11f357ac2d8
mazhar <mazharulislam611@gmail.com>
2023-10-17 22:59:45 +0600

    pass box update

---
+++ index.js
@@ -0,0 +1,6 @@
+let passInput = document.getElementById("psw");
+let letter = document.getElementById("letter");
+let capital = document.getElementById("capital");
+let number = document.getElementById("number");
+let length = document.getElementById("length");
+
